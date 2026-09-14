e2b5be2d215f60550473767f6625f91c333309e7
Shhavarie <w6ghfybp2r@privaterelay.appleid.com>
2026-09-15 02:08:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1340,7 +1340,7 @@ async function startGame(role) {
                 initialCard
             );
             initialCard.x = 350;
-            initialCard.y = 550;
+            initialCard.y = 500;
 
             player.board.push(initialCard);
 
@@ -2317,7 +2317,7 @@ function renderBoard() {
                 : sourceX;
 
             const displayY = isOpponent
-                ? boardHeight - sourceY - cardHeight + 50
+                ? boardHeight - sourceY - cardHeight + 35
                 : sourceY;
 
             cardElement.style.left = `${displayX}px`;
